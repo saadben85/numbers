487ea807648d21e26a9feddf046729ecdd5ec580
saadben85 <161115749+saadben85@users.noreply.github.com>
2026-03-21 18:13:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
-const CACHE = 'num-trainer-v1';
-const FILES = ['./', './index.html', './manifest.json', './icon-512.png'];
+const CACHE_NAME = 'num-trainer-v2';
+const FILES = [
+  '/numbers/',
+  '/numbers/index.html',
+  '/numbers/manifest.json',
+  '/numbers/icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
